eb2e77863ed1cf0d466c4f9a0b846f5badbf839e
Tejas P <tejasteju0726@gmail.com>
2024-10-14 22:39:56 +0530

    Add files via upload

---
+++ program with if,else,elif.py
@@ -0,0 +1,15 @@
+time = input("Time :")
+if time =="5am":
+    print("It's time wake up !")
+elif time =="5:30am":
+    print("It's time to study !")
+elif time =="6:30am":
+    print("It's time to excise !")
+elif time =="8am":
+    print("It's breakfast time!")
+elif time =="1pm" :
+    print("It's lunch time!")
+elif time =="8pm":
+    print("It's dinner time!")
+
+
